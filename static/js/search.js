ea60d1f8020f786398d9dd88842a5a7be86da6d0
ryouya <r.hatafuku.sys22@morijiyobi.ac.jp>
2023-12-08 09:32:20 +0900

    プロフィール編集機能、画像表示完成(エラーパターン以外)

--- static/js/search.js
+++ static/js/search.js
@@ -4,12 +4,6 @@ document.querySelector('.search-button').addEventListener('click', function() {
     var resultsContainer = document.querySelector('.results-container');
     resultsContainer.innerHTML = '';
 
-    document.querySelector('.search-input').addEventListener('keypress', function(e) {
-        if (e.key === 'Enter') {
-            executeSearch();
-        }
-    });
-
     fetch('/community_user_search', {
         method: 'POST',
         headers: {
@@ -35,8 +29,13 @@ document.querySelector('.search-button').addEventListener('click', function() {
                 let resultItem = document.createElement('a');
                 resultItem.classList.add('result-item');
                 resultItem.href = `/user_detail/${user.user_id}`; // ユーザー詳細ページへのリンク
+                
+                // アイコンURLの生成
+                let iconUrl = 'https://oshimore.s3.ap-northeast-1.amazonaws.com/img/';
+                iconUrl += (user.icon_url && user.icon_url !== 'user_icon.png') ? user.icon_url : 'user_icon.png';
+
                 resultItem.innerHTML = `
-                    <div class="result-icon"><img src="${user.icon_url || 'default_icon.png'}"></div>
+                    <div class="result-icon"><img src="${iconUrl}" alt="User Icon"></div>
                     <div class="result-name">${user.account_name}</div>
                     <div class="result-id">@${user.user_id}</div>
                 `;
